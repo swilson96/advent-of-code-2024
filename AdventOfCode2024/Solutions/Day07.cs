4647dce3011090e404f2287431cfd27cb7fce753
Simon Wilson <simon.wilson@hudl.com>
2024-12-27 07:35:22 +0000

    day 7

--- AdventOfCode2024/Solutions/Day07.cs
+++ AdventOfCode2024/Solutions/Day07.cs
@@ -4,25 +4,33 @@ namespace AdventOfCode2024.Solutions;
 
 public class Day07 : IAdventSolution
 {
-    public object PartOne(string input) => input
+    public object PartOne(string input) => SumValidAccordingTo(input, 2);
+    
+    public object PartTwo(string input) => SumValidAccordingTo(input, 3);
+    
+    public long SumValidAccordingTo(string input, int numOps) => input
         .Split(Environment.NewLine)
-        .Where(CanBeValid)
+        .Where(l => CanBeValid(l, numOps))
         .Select(l => l.Split(": ")[0])
         .Select(long.Parse)
         .Sum();
 
-    public bool CanBeValid(string line)
+    public bool CanBeValid(string line, int numOps)
     {
         var parts = line.Split(": ");
         var target = long.Parse(parts[0]);
         var terms = parts[1].Split(" ").Select(int.Parse).ToList();
 
         var gaps = terms.Count - 1;
-        for (var flags = 0; flags < 1 << gaps; ++flags)
+        for (var flags = 0; flags < Math.Pow(numOps, gaps); ++flags)
         {
             var result = terms
-                .Select((t, i) => new Tuple<int, int>(t, ReadBit(flags, i-1)))
-                .Aggregate(0L, (a, b) => b.Item2 == 0 ? a + b.Item1 : a * b.Item1);
+                .Select((t, i) => new Tuple<int, int>(t, ReadBit(flags, i-1, numOps)))
+                .Aggregate(0L, (a, b) => b.Item2 == 0 
+                    ? a + b.Item1 
+                    : b.Item2 == 1 
+                        ? a * b.Item1
+                        : long.Parse(a.ToString() + b.Item1.ToString()));
             if (result == target)
             {
                 return true;
@@ -32,12 +40,16 @@ public class Day07 : IAdventSolution
         return false;
     }
     
-    private int ReadBit(long n, int k)
+    private int ReadBit(long n, int k, int numOps)
     {
         if (k < 0) return 0; // specific to this algorithm
-        var temp = n >> k;
-        return (int) temp & 1;
-    }
+        var temp = n / Math.Pow(numOps, k);
+        var ret = (int) temp % numOps;
+        if (ret < 0)
+        {
+            ret += numOps;
+        }
 
-    public object PartTwo(string input) => 0;
+        return ret;
+    }
 }
